e4c2aa0be72f46c36d8e5a116fe63c33ac151365
TacocaTNL <garslade359@gmail.com>
2017-03-02 22:52:21 +0100

    Create login-form.php

---
+++ login-form.php
@@ -0,0 +1,25 @@
+<?php
+
+$email = htmlentities($_POST['email']);
+$password = htmlentities($_POST['password']);
+
+include 'db.php';
+
+$sql = "SELECT * FROM `users` WHERE `email`='$email'";
+$result = mysqli_query($conn, $sql);
+$count = mysqli_num_rows($result);
+if ($count == 1) {
+	$row = mysqli_fetch_array($result);
+	$salt = $row['salt'];
+	$saltPass = $salt . $password;
+	$encrypt = hash('sha256', $saltPass);
+	if ($row['password'] == $encrypt) {
+		header("Location: index.php");
+	} else {
+		//somethng
+	}
+} else {
+		//something
+}
+
+?>
